fcdfcd3170764078939fe5d2190baa0b2bcaf23a
Unknown <pekka.alaluukas@hotmail.com>
2016-10-31 13:23:23 +0200

    esimerkki2 valmis

---
+++ yhteys.php
@@ -0,0 +1,13 @@
+<?php
+$palvelin="mysql:host=localhost; dbname=esim2";
+try {
+	$db=new PDO($palvelin,"phpuser","phppass");
+	//echo "Yhteys OK";
+
+} catch (Exception $e) {
+	echo "Tietokantaan ei saatu yhteyttä, ota yhteys ylläpitoon";
+	
+}
+
+
+?>
