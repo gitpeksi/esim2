bc1d9d78a5671e68068ef8d93e9dcfb06cbac7c6
Unknown <pekka.alaluukas@hotmail.com>
2016-11-01 09:45:33 +0200

    Login form tehty

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php include "menu.php";?>
+<form method="POST" action="login.php">
+<TABLE BORDER=0>
+	<tr><td>tunnus</td> <td><input type="text" name="tunnus"></td></tr>
+	<tr><td>salasana</td> <td><input type="password" name="sala"></td></tr>
+	<tr><td></td><td><input type="submit" name="nappi" value="Kirjaudu"></td></tr>
+</TABLE>
+</form>
+
+<?php
+if(isset($_POST['nappi'])) {
+	$oikeatunnus="jeppe";
+	$oikeasalasana="test";
+	if($_POST['tunnus']==$oikeatunnus) {
+		if($_POST['sala']==$oikeasalasana) {
+			//oikea tunnus ja salasana
+			header('Location:oma.php');
+		}
+		else {
+			echo "Tunnus ja salasana eivät täsmää";
+		}
+	}
+	else {
+		echo "Tunnus ja salasana eivät täsmää";
+	}
+}
+?>
+
+
+<?php include "footer.php";?>
